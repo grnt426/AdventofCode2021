d4aad836739fca40c4a1e76c8880f6ea902bcc77
Grant Kurtz <kurtz.grant@gmail.com>
2021-12-21 09:23:45 -0500

    Day 5 WIP

---
+++ src/com/grnt426/day5/Day5.kt
@@ -0,0 +1,79 @@
+package com.grnt426.day5
+
+import java.io.File
+
+class Day5 {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            /**
+             * Solution:
+             *
+             * Previous guesses: 18916 (too high)
+             */
+            println("Part 1 Result: " + Day5().findDangerousOverlap("input/day5/input"))
+        }
+    }
+
+    class Coord(startData: List<String>, endData: List<String>) {
+
+        val start = IntArray(2)
+        val end = IntArray(2)
+
+
+        init {
+            start[0] = startData[0].toInt()
+            start[1] = startData[1].toInt()
+            end[0] = endData[0].toInt()
+            end[1] = endData[1].toInt()
+        }
+    }
+
+    private fun findDangerousOverlap(s: String): Int {
+        val markedSpots = HashSet<String>()
+        var hits = 0
+
+        File(s).forEachLine {
+            val pair = it.split(" -> ")
+            val coord = Coord(pair[0].split(","), pair[1].split(","))
+
+            var start = 0
+            var end = 0
+            var staticAxis = 1
+
+            when {
+                coord.start[0] < coord.end[0] -> {
+                    start = coord.start[0]
+                    end = coord.end[0]
+                }
+                coord.start[0] > coord.end[0] -> {
+                    start = coord.end[0]
+                    end = coord.start[0]
+                }
+                coord.start[1] < coord.end[1] -> {
+                    start = coord.start[1]
+                    end = coord.end[1]
+                    staticAxis = 0
+                }
+                coord.start[1] > coord.end[1] -> {
+                    start = coord.end[1]
+                    end = coord.start[1]
+                    staticAxis = 0
+                }
+                else -> {
+                    println("ERROR! Something went wrong in processing")
+                }
+            }
+
+            for(i in start..end) {
+                val x = if(staticAxis == 0) coord.start[0] else i
+                val y = if(staticAxis == 1) coord.start[1] else i
+                if(!markedSpots.add("$x,$y")) hits++
+            }
+        }
+
+        return hits
+    }
+}
